d4fdff5b9fd505f06327be4f919f6fcbb1c56867
Rochim Farul Noviyan <rochim.noviyan@gmail.com>
2025-01-27 21:59:53 +0700

    Fix kubernetes using dangling images
    This is because I use the same tag. The ideal solution whould be
    generate the tag.

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+microk8s ctr images rm localhost:32000/demo-remote-debug-java:0.0.1
 docker tag demo-remote-debug-java:0.0.1 localhost:32000/demo-remote-debug-java:0.0.1
 docker push localhost:32000/demo-remote-debug-java:0.0.1
+docker rmi localhost:32000/demo-remote-debug-java:0.0.1

40292426b9a8cac94c041a24dd02c582637c3dc1
Rochim Farul Noviyan <rochim.noviyan@gmail.com>
2025-01-28 15:48:00 +0700

    Add flag to switch jvm remote debug

--- scripts/entry-point.sh
+++ scripts/entry-point.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-java -jar demo-0.0.1-SNAPSHOT.jar
+if [ "$DEBUG_MODE" = "1" ]; then
+    echo "DEBUG_MODE is active"
+    java -Xdebug -Xrunjdwp:transport=dt_socket,address=8888,server=y,suspend=n -jar demo-0.0.1-SNAPSHOT.jar
+else
+    echo "DEBUG_MODE is inactive"
+    java -jar demo-0.0.1-SNAPSHOT.jar
+fi
